1675dbd691bdf2fb38cef6c47326c6963dab6304
mauricio.sierrac <2376@holbertonschool.com>
2020-12-08 22:23:07 +0000

    fix  task8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def islower(char):
-
-
-if ord(char) > 96 and ord(char) < 123:
-    return True
-else:
-    return False
+def islower(c):
+        if ord(c) > 96 and ord(c) < 123:
+                return True
+        else:
+                return False
