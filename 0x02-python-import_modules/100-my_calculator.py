6e37d6a5b4e42b80092e9515b90a58ed481a7f9c
mauricio.sierrac <2376@holbertonschool.com>
2020-12-10 23:11:26 +0000

    upload advanced tas100 :snake:

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,24 +4,24 @@ from sys import argv
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    n = len(sys.argv)
+    n = len(argv)
 
     if n == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
+        a = int(argv[1])
+        b = int(argv[3])
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    if sys.argv[2] == '+':
+    if argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
+    elif argv[2] == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
+    elif argv[2] == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
+    elif argv[2] == '/':
         print("{} / {} = {}".format(a, b, div(a, b)))
-        sys.exit(0)
+        exit(0)
     elif argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
