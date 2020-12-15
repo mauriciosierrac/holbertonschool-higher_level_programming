b1e6c49babfc3563eba57c86d9091b65765d4b1d
mauricio.sierrac <2376@holbertonschool.com>
2020-12-15 23:52:20 +0000

    fix condition :snake:

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,11 +2,19 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
+    len1 = len(tuple_a)
+    len2 = len(tuple_b)
 
-    for i in range(len(tuple_a)):
+    if len1 > 2:
+        len1 = 2
+
+    if len2 > 2:
+        len2 = 2
+
+    for i in range(len1):
         a[i] += tuple_a[i]
 
-    for j in range(len(tuple_b)):
+    for j in range(len2):
         b[j] += tuple_b[j]
 
     return (a[0] + b[0], a[1] + b[1])
