976c50041e54e77f3b5cb2ef9e4af54cc8b55cf6
mauricio.sierrac <2376@holbertonschool.com>
2020-12-15 17:19:06 +0000

    fix return :snake:

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     lenght = len(my_list)
     if idx < 0 or idx > lenght:
-        return (None)
+        return (my_list)
     else:
         for i in list(range(lenght)):
             if idx == i:
