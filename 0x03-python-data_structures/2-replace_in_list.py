c008551d860aad7aae71c0eba12f169f1547cae3
mauricio.sierrac <2376@holbertonschool.com>
2020-12-15 15:46:08 +0000

    upload task2 :snake:

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    lenght = len(my_list)
+    if idx < 0 or idx > lenght:
+        return (None)
+    else:
+        for i in list(range(lenght)):
+            if idx == i:
+                my_list[i] = element
+    return (my_list)
+                
