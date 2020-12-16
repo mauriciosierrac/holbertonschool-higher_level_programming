516c8385aac808740f60f7bfea2f66079c0bd938
mauricio.sierrac <2376@holbertonschool.com>
2020-12-16 00:56:55 +0000

    task 10 :snake:

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     if my_list:
         for i in my_list:
             if i % 2 == 0:
-                resul.append(0)
+                resul.append(True)
             else:
-                resul.append(1)
+                resul.append(False)
         return (resul)
