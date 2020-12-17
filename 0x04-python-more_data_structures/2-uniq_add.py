ac24f0fc196685880fb1278918474d04a5c185db
mauricio.sierrac <2376@holbertonschool.com>
2020-12-17 18:32:28 +0000

    task2 :snake:

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq = set(my_list)
-    long = len(uniq)
-    res = 0
-    for i in range(long + 1):
-        res = res + i
+    res = sum(set(my_list), 0)
     return(res)
