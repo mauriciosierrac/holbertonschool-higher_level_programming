9c3d9013f4fc5fc694556e0ecf3fdd713e36ac7e
mauricio.sierrac <2376@holbertonschool.com>
2021-01-04 19:40:59 +0000

    fix task2 :snake:

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     cont = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                cont = cont + 1
-                print('{}'.format(my_list[i]), end="")
-    except (ValueError, TypeError):
-        return
+
+    for i in range(x):
+        try:
+            cont = cont + 1
+            print('{:d}'.format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
 
     print()
     return (cont)
