2a95e7ee728598ba3a5c124f35f96ab217642f66
mauricio.sierrac <2376@holbertonschool.com>
2021-01-04 17:35:43 +0000

    task 0 :snake:

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0, x):
+        for i in range(x):
             print('{}'.format(my_list[i]), end="")
         print()
-        return (i + 1)
+        return (x)
     except (TypeError, IndexError):
         print()
         return (i)
