36ff81ffa332220627146bd4de5a31835f0556bf
mauricio.sierrac <2376@holbertonschool.com>
2021-01-04 19:21:33 +0000

    task4 :snake:

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,10 +6,10 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             resul = my_list_1[i] / my_list_2[i]
         except (TypeError, ValueError):
-            print('Wrong type')
+            print("wrong type")
             resul = 0
         except ZeroDivisionError:
-            print('division by 0')
+            print("division by 0")
             resul = 0
         except IndexError:
             print('out of range')
