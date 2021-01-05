76c8a2049cc92272774fa7c8f29810012f5b92c7
mauricio.sierrac <2376@holbertonschool.com>
2021-01-05 21:26:37 +0000

    task4 :snake:

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,7 @@
 ''' class square defines a square by 1-square.py'''
 
 
-class Square:
+class Square():
     ''' the method validate is size is a integer and calculate square value of size '''
 
     def __init__(self, size=0):
@@ -23,10 +23,14 @@ class Square:
     @property
     def size(self):
         '''documentation'''
-       return self.__size
+        return self.__size
    
     #a setter function 
     @size.setter
     def size(self, value):
         '''documentation'''
         self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
