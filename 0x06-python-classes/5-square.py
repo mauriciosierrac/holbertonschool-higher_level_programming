4a1c7c7bdb0b6a138095d433d84952063daa6165
mauricio.sierrac <2376@holbertonschool.com>
2021-01-05 21:43:08 +0000

    task5 :snake:

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' class square defines a square by 1-square.py'''
+
+
+class Square():
+    ''' the method validate is size is a integer and calculate square value '''
+
+    def __init__(self, size=0):
+        '''documentation'''
+        self.__size = size
+
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+
+        if self.__size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        '''documentation'''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''documentation'''
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print('{}'.format("#" * self.__size))
+
+    @property
+    def size(self):
+        '''documentation'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''documentation'''
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
