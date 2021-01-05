7b50468c69de7ac002cf26265149c7afcc1a948b
mauricio.sierrac <2376@holbertonschool.com>
2021-01-05 19:44:26 +0000

    task1 :snake:

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' This class defines a square based on 0-square.py'''
+
+
+class Square:
+    ''' Define size of square'''
+
+    def __init__(self, __size):
+        self.__size = __size
