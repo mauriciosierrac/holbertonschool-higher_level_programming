08a2a5542cb86c9676409341ccba307e81162fb6
mauricio.sierrac <2376@holbertonschool.com>
2021-01-06 04:19:18 +0000

    task6 :snake:

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+''' class square defines a square by 1-square.py'''
+
+
+class Square():
+    ''' the method validate is size is a integer and calculate square value '''
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''documentation'''
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        '''documentation'''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''documentation'''
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("".join([" " for k in range(self.__position[0])]), end="")
+            print("".join(["#" for l in range(self.__size)]))
+
+    @property
+    def position(self):
+        '''documentation'''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''documentation'''
+        if type(value) is not tuple or len(value) != 2 or \
+        type(value[0]) is not int or value[0] < 0 or \
+        type(value[1]) is not int or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
+
+    @property
+    def size(self):
+        '''documentation'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''documentation'''
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError('size must be an integer')
+        elif self.__size < 0:
+            raise ValueError('size must be >= 0')
