b3fbe7da83dffd608b2ee8543d546f0221443e18
mauricio.sierrac <2376@holbertonschool.com>
2021-01-11 23:29:38 +0000

    task9 :snake:

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -95,4 +95,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         ''' that method return a instance of rectangle'''
-        return cls(size, size)
+        return Rectangle(size, size)
