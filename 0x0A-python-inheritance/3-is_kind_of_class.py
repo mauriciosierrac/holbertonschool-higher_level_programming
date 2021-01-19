7c34d3513f7ca3491953b45347fb6080a0288f29
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 23:31:23 +0000

    fix :snake:

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+''' comment '''
+
+
 def is_kind_of_class(obj, a_class):
     '''
     that function return true if the object is a class instance
     or false if the object is a inheritance class
     '''
-
     return isinstance(obj, a_class)
