47bb7da673a422ac9fe9bb29c8225f0d183e0604
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 16:36:04 +0000

    task1 :snake:

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''comments'''
+
+
+class MyList(list):
+    '''super class, define MyList'''
+
+    def print_sorted(self):
+        '''that method sorted the list'''
+        print(sorted(self))
