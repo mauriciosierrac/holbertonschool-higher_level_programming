64c7f9fd6208d4561031a58869f7e8f959e2c92c
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 23:41:37 +0000

    fix :snake:

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,21 +2,7 @@
 '''comment'''
 
 
-class BaseGeometry:
-    '''super class'''
-
-    def area(self):
-        '''this method return a exception message'''
-
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        '''that method validate a value parameter'''
-
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -24,7 +10,8 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         '''method of instance'''
-        self.__width = width
-        self.__height = height
+
         self.integer_validator('width', width)
+        self.__width = width
         self.integer_validator('height', height)
+        self.__height = height
