debe536d228b822cf5611330facfc5dc084f2cad
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 17:18:06 +0000

    task2 :snake:

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    '''
+    that function return true or false if this object
+    is a class instance
+    '''
+    if type(obj) is a_class:
+        return True
