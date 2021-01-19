f866d1b50e7377e47a790721735cde16799458c6
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 23:33:08 +0000

    fix :snake:

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+'''comment '''
+
+
 def inherits_from(obj, a_class):
     '''
     that function return true if the object is a instance
     from inheritance class
     '''
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
+    return issubclass(type(obj), a_class) and type(obj) != a_class
