3ea6aa796a43d5875576267625110fe94a3c86d2
mauricio.sierrac <2376@holbertonschool.com>
2021-01-19 23:25:25 +0000

    fix :snake:

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,6 @@ def is_same_class(obj, a_class):
     that function return true or false if this object
     is a class instance
     '''
-    if type(obj) is a_class:
+    if obj.__class__ == a_class:
         return True
     return False
