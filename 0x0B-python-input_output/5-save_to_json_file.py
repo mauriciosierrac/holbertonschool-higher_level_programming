4f94c0d2605c9785d7dd73538635c2c92a591d25
mauricio.sierrac <2376@holbertonschool.com>
2021-01-20 17:30:45 +0000

    task5 :snake:

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''comment'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''write in json object'''
+    with open(filename, 'w') as outfile:
+        return json.dump(my_obj, outfile)
