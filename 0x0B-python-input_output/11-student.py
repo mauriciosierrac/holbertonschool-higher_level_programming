387661a04f44d32ae4ab1e5c8260ac7054a82109
mauricio.sierrac <2376@holbertonschool.com>
2021-01-21 04:41:31 +0000

    fix :snake:

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''comment'''
+
+
+class Student:
+    '''
+    class that define a student'''
+
+    def __init__(self, first_name, last_name, age):
+        '''instance to class Student'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''return a representation of diccionary'''
+        if attrs is None:
+            return self.__dict__
+        new = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new[key] = value
+        return new
+
+    def reload_from_json(self, json):
+        '''reload from Json'''
+        self.__dict__.update(json)
