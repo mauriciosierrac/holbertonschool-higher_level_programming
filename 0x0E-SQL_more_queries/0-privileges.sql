1e331b0bd537cbe6b128c9f364e4b790325b3ea6
mauricio.sierrac <2376@holbertonschool.com>
2021-03-02 19:23:10 +0000

    CREATE TASK FILES :beers: :pizza:

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list all privileges of the user 0d_1 y 0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
