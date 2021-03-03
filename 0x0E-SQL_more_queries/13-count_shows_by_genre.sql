f3a1792a83e63ace4594faa4071e17b3d579f446
mauricio.sierrac <2376@holbertonschool.com>
2021-03-03 00:22:54 +0000

    CREATE TASK FILES :beers: :pizza:

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,9 @@
 -- lists all genres from database and display the number of shows linked to each
-SELECT `tv_genres`.`name` as `genre`, COUNT(*) AS `number_of_shows`
-FROM `tv_genres` INNER JOIN `tv_show_genres`
-ON `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+-- second cooment
+
+SELECT `tv_genres`.`name` as genre, COUNT(*) AS number_of_shows
+FROM `tv_genres`
+INNER JOIN `tv_show_genres`
+ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
 GROUP BY `tv_show_genres`.`genre_id`
 ORDER BY `number_of_shows` DESC;
