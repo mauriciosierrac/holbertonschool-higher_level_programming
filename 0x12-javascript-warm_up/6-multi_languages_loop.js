9fb3c5ceb534bb3c5651b5d4e091df70100eae58
mauricio.sierrac <2376@holbertonschool.com>
2021-03-22 18:23:25 +0000

    create task06

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myList = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = myList.length;
+let i = 0;
+let p = 0;
+while (i < len) {
+  console.log(myList[p]);
+  i++;
+  p++;
+}
