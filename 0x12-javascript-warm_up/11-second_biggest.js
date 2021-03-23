436d2d899361ff1e528b2f69ad8306f7d18575fb
mauricio.sierrac <2376@holbertonschool.com>
2021-03-23 01:02:36 +0000

    fix task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const len = process.argv.length;
-
-if (len <= 3) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  const nums = process.argv.slice(2, len).map(Number).sort().reverse();
-  console.log(parseInt(nums[1]));
+  const list = process.argv.sort().reverse();
+  console.log(parseInt(list[1]));
 }
