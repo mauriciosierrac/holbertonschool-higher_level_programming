badcbc6b882030d9a69dcacbbf9014cf3b9e8ba0
mauricio.sierrac <2376@holbertonschool.com>
2021-03-26 17:25:22 +0000

    migration

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''that module connect with my db'''
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    myUser = argv[1]
+    myPswd = argv[2]
+    myDb = argv[3]
+    
+
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user=myUser, passwd="", db=myDb, charset="utf8")
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
