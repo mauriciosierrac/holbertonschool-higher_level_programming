e315d86dbe41b01f424dc31326c71630652fbba7
mauriciosierrac <2376@holbertonschool.com>
2021-03-27 18:07:33 -0500

    update and fixed pep8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''that module connect with my db'''
+
 from sys import argv
 import MySQLdb
 
@@ -8,14 +9,14 @@ if __name__ == '__main__':
     myPswd = argv[2]
     myDb = argv[3]
 
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=myUser, passwd=myPswd, db=myDb, charset="utf8")
+    cur = conn.cursor()
 
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=myUser, passwd=myPswd, db=myDb, charset="utf8")
-cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
 
-cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
