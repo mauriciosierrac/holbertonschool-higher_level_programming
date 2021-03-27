c8422567dc9a2053983abf392ed549e6c9f4efc9
mauriciosierrac <2376@holbertonschool.com>
2021-03-27 18:48:28 -0500

    fixed pep8

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+'''module make join'''
+
 from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+                           user=argv[1], passwd=argv[2],
+                           db=argv[3], charset="utf8")
 
     cur = conn.cursor()
 
-    cur.execute(
-        "SELECT * FROM states JOIN cities ON  states.id = cities.state_id ORDER BY cities.id ASC")
+    cur.execute("SELECT * FROM states JOIN cities\
+        ON  states.id = cities.state_id ORDER BY cities.id ASC")
 
     query = cur.fetchall()
     x = ''
