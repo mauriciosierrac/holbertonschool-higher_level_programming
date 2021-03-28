6593234b16b9872224c9f566fa03282558ff19fd
mauriciosierrac <2376@holbertonschool.com>
2021-03-28 13:38:51 -0500

    update task10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,13 +12,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = sessionmaker(bind=engine)
 
-    Base.metadata.create_all(engine)
-
-    myQuery = session.query(State.id, State.name).filter(
-        State.name == sys.argv[4]).all()
+    myQuery = session().query(State).filter(State.name == sys.argv[4]).all()
 
     if myQuery:
         for i in myQuery:
@@ -26,3 +22,5 @@ if __name__ == "__main__":
                 print('{}'.format(i.id))
     else:
         print('Not Found')
+
+    session().close()
