f25e0723d455431ad6a12beaf05dbc0959d08e0c
mauriciosierrac <2376@holbertonschool.com>
2021-03-28 15:52:10 -0500

    update task13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''that method delete the objets with containt 'a' in state table'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    myQuery = session.query(State).filter(State.name.like('%a%')).all()
+
+    for i in myQuery:
+        session.delete(i)
+
+    session.commit()
