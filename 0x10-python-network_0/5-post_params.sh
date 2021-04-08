c1fe2c3aad62182ad28dd165a3f1e52ad15cef44
mauricio.sierrac <2376@holbertonschool.com>
2021-04-08 20:56:49 +0000

    fix -s flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script send POST request to the passed, and display the body of the response
-curl -s -X POST -d "email=hr@holbertonschool&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
