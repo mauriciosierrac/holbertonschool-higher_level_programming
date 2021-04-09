64f9eb2a22444ce0a57b4214f49a837eaf0dd4f9
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 16:17:14 -0500

    fix space after -

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 '''This function fetches type, content and decode from a url'''
 
-import urllib.request
+import urllib.request as request
 
 if __name__ == "__main__":
-    '''function fetche'''
-
-    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
+    """My status"""
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
