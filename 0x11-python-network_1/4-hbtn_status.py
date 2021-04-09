e841ddc6aa811536e3e6b00b014689a75d4214eb
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 23:22:08 +0000

    update task 04

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''Write a Python script that fetches https://intranet.hbtn.io/status'''
+
+import requests
+
+if __name__ == '__main__':
+    '''fetches method'''
+
+    req = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
