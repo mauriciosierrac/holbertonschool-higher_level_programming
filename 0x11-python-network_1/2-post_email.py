74eefaebe3d383feb5ad6f2bf5e1193ffc59d226
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 17:15:20 -0500

    update task02

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python3
+'''Write a Python script that takes in a URL'''
+
+from sys import argv
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    ''' send POST '''
+    url = argv[1]
+    arg = {"email": argv[2]}
+    mail = urllib.parse.urlencode(arg).encode('ascii')
+
+    req = urllib.request.Request(url, mail)
+    with urllib.request.urlopen(req) as response:
+        r = response.read().decode('utf-8')
+        print(r)
