e585281390ac47f7661f41abfd1084d3c25bb435
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 22:57:43 +0000

    upddate task03

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Write a Python script that takes in a URL'''
+
+from sys import argv
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    '''send a request to the URL and displayst the body of the response'''
+
+    url = argv[1]
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            r = response.read().decode('ascii')
+            print(r)
+    except urllib.error.HTTPError as error:
+        print('Error code:', error.code)
