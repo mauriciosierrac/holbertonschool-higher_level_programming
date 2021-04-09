f100409863e1210324ac4668bca5d8a7a6c5873c
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 15:58:22 -0500

    fix space after -

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
         content = response.read()
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
