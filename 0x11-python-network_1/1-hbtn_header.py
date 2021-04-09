a6d40916c2b8a7562b5d75d29562930c9fe05f7b
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 16:22:57 -0500

    fix main function

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,11 @@ of x-request-id variable found in the header'''
 
 import urllib.request
 from sys import argv
-import requests
-from requests.models import Request
 
-url = argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    r = response.headers
-    print(r['X-Request-Id'])
+if __name__ == "__main__":
+    """My status"""
+    url = argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        r = response.headers
+        print(r['X-Request-Id'])
