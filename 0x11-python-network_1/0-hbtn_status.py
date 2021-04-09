30b90fd8682108f5957c79176da7c4c77a06ce5d
mauriciosierrac <2376@holbertonschool.com>
2021-04-09 14:49:37 -0500

    update task 01

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+'''This function fetches type, content and decode from a url'''
+
 import urllib.request
 
-# with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-#     html = response.read()
-#     print(html)
 
-response = request.get('http://intranet.hbtn.io/status')
-print(response)
+with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
+    content = response.read()
+
+
+print('Body response:')
+print('\t-type: {}'.format(type(content)))
+print('\t-content: {}'.format(content))
+print('\t-utf8 content: {}'.format(content.decode('utf-8')))
