541f0c71eb4027336b6a9a65222a8012b5274466
mauriciosierrac <2376@holbertonschool.com>
2021-04-10 02:45:03 +0000

    update task9= 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+'''method get API github'''
+
 
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    '''method get API github'''
+    
     data = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(data.json().get('id'))
