961c4884e087328496f13853e3125136bc57b3b2
mauriciosierrac <2376@holbertonschool.com>
2021-04-10 02:45:40 +0000

    update task9= 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    
+
     data = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(data.json().get('id'))
