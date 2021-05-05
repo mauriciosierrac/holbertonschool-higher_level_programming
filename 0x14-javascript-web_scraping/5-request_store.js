413fdb08100e7174b438607387b07282cb7fd81c
mauriciosierrac <2376@holbertonschool.com>
2021-05-04 20:24:47 -0500

    fixed bad return

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
 const pathFile = process.argv[3];
-const fs = require('fs');
-const codi = 'utf8';
 
-request(url, function (body) {
-  fs.writeFile(pathFile, body, codi, function (error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(url > pathFile);
-    }
-  });
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(pathFile, body, 'utf-8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
